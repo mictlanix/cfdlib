c9afccecc37d41a9a464a1f2b575c1c13af78a31
Eddy Zavaleta <eddy@mictlanix.org>
2012-04-03 16:06:36 -0500

    version number updated

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,27 +1,28 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
-
 [assembly: AssemblyTitle("Mictlanix CFDv2 Library")]
 [assembly: AssemblyDescription("Mexican Digital Invoicing (CFDv2) Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mictlanix")]
 [assembly: AssemblyProduct("CFDv2 Library")]
-[assembly: AssemblyCopyright("Copyright © Eddy Zavaleta, Mictlanix and contributors.")]
+[assembly: AssemblyCopyright("Copyright © 2012 Eddy Zavaleta, Mictlanix and contributors.")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("es-MX")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f64af0f0-7db3-11e1-b0c4-0800200c9a66")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
